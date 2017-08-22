f702fb8cce7ba25c71066a01a0f5e147c219a40c
seoungkwang.kim <kim-sk@sk.com>
2017-08-22 11:42:58 +0900

    add hello world menu

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,47 +1,46 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-import { RoutingModule } from './routing.module';
-import { SharedModule } from './shared/shared.module';
-import { CatService } from './services/cat.service';
-import { UserService } from './services/user.service';
-import { AuthService } from './services/auth.service';
-import { AuthGuardLogin } from './services/auth-guard-login.service';
-import { AuthGuardAdmin } from './services/auth-guard-admin.service';
 import { AppComponent } from './app.component';
-import { CatsComponent } from './cats/cats.component';
-import { AboutComponent } from './about/about.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { AccountComponent } from './account/account.component';
-import { AdminComponent } from './admin/admin.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
+
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
+import { AsideToggleDirective } from './shared/aside.directive';
+import { BreadcrumbsComponent } from './shared/breadcrumb.component';
+
+// Routing Module
+import { RoutingModule } from './routing.module';
+
+// Layouts
+import { FullLayoutComponent } from './layouts/full-layout.component';
+
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ChartsModule
+  ],
   declarations: [
     AppComponent,
-    CatsComponent,
-    AboutComponent,
-    RegisterComponent,
-    LoginComponent,
-    LogoutComponent,
-    AccountComponent,
-    AdminComponent,
+    FullLayoutComponent,
+    NAV_DROPDOWN_DIRECTIVES,
+    BreadcrumbsComponent,
+    SIDEBAR_TOGGLE_DIRECTIVES,
+    AsideToggleDirective,
     NotFoundComponent
   ],
-  imports: [
-    RoutingModule,
-    SharedModule
-  ],
-  providers: [
-    AuthService,
-    AuthGuardLogin,
-    AuthGuardAdmin,
-    CatService,
-    UserService
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  bootstrap: [AppComponent]
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap: [ AppComponent ]
 })
-
 export class AppModule { }
--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as morgan from 'morgan';
-import * as mongoose from 'mongoose';
+// import * as mongoose from 'mongoose';
 import * as path from 'path';
 
 import setRoutes from './routes';
@@ -16,10 +16,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
-mongoose.connect(process.env.MONGODB_URI);
-const db = mongoose.connection;
-(<any>mongoose).Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI);
+// const db = mongoose.connection;
+// (<any>mongoose).Promise = global.Promise;
 
+/*
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
@@ -34,6 +35,19 @@ db.once('open', () => {
     console.log('Angular Full Stack listening on port ' + app.get('port'));
   });
 
+});
+*/
+
+console.log('Connected to MongoDB');
+
+setRoutes(app);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Angular Full Stack listening on port ' + app.get('port'));
 });
 
 export { app };
